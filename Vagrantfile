f270a01587d25df69322352d77583a6980c11619
Jon Masters <Jonathan_masters77@yahoo.co.uk>
2016-12-15 17:41:26 +0000

    development.conf alts

--- Vagrantfile
+++ Vagrantfile
@@ -33,6 +33,6 @@ Vagrant.configure(2) do |config|
     "www.humberevents.dev"
   ]
 
-config.vm.provision "shell", path: "scripts/vagrant-provision.sh"
+ config.vm.provision "shell", path: "scripts/vagrant-provision.sh"
 
 end
--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -6,10 +6,10 @@
 sudo cp /vagrant/config/environments/development.conf /etc/apache2/sites-available/humberevents.dev.conf
 sudo rm -rf /var/www
 sudo mkdir -p /var/www
-sudo ln -fs /vagrant /var/www/humberevents
+sudo ln -fs /vagrant /var/www/humberevents.dev
 
 # Enable new virtual host
-sudo ln -fs /etc/apache2/sites-available/humberevents.conf /etc/apache2/sites-enabled/humberevents.conf
+sudo ln -fs /etc/apache2/sites-available/humberevents.dev.conf /etc/apache2/sites-enabled/humberevents.dev.conf
 
 # Restart Apache
 sudo service apache2 restart
@@ -18,10 +18,11 @@ sudo service apache2 restart
 mysql -u root -proot -e "create database if not exists wordpress"
 #mysql -u root -proot wordpress < /vagrant/config/bootstrap.sql
 
-
+# Install Composer Dependencies
+php /usr/local/bin/composer.phar update --working-dir="/var/www/humberevents.dev" --no-interaction
 
 # Node
 npm -v
 npm install -g grunt-cli
 npm install -g bower
-npm install --prefix /html/app/themes/humber-lincolnshire-eventssss/
+npm install --prefix /html/app/themes/humber-lincolnshire-events/
